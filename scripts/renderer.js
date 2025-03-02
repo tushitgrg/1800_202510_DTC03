7641d752478b6d8e115de60a230a95878fe6a8ae
Tushit <tushitgarg123@gmail.com>
2025-03-02 15:01:45 -0800

    Add logout page

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,5 +1,36 @@
-$(document).ready(function(){
+function renderPage(){
+
+     firebase.auth().onAuthStateChanged(function (user) {
+        
+        if (user) {   
+        
+            console.log($('#header').load('../components/login_header.html'));
+            console.log($('#footer').load('../components/footer.html'));
+            document.getElementById("logoutbtn").addEventListener('click',()=>{
+                alert()
+            })
+        } else {
+            // No user is signed in.
+            console.log($('#header').load('../components/header.html'));
+            console.log($('#footer').load('../components/footer.html'));
+
+        }
+    });
     
-    $("#header").load("../components/header.html");
-    $("#footer").load("../components/footer.html");
-  });
+  
+     
+}
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
+
+renderPage()
+ 
+
+  
