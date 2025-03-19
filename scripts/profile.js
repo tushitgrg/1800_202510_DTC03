69cf6ab0cfd10b0691020d58cf2e215aec00ac6c
Tushit <tushitgarg123@gmail.com>
2025-03-19 11:50:01 -0700

    Add image upload

--- scripts/profile.js
+++ scripts/profile.js
@@ -30,6 +30,9 @@ function populateUserInfo() {
                         if (data.aboutMe) {
                             document.getElementById("aboutMeInput").value = data.aboutMe;
                         }
+                        if(data.avatar) {
+                   document.getElementById("avatarImg").setAttribute("src",data.avatar)
+                        }
                     }
                 })
                 .catch(error => console.error("Error getting user data:", error));
@@ -47,19 +50,32 @@ function editUserInfo() {
     document.getElementById('pronounsInput').removeAttribute("disabled");
     document.getElementById('aboutMeInput').removeAttribute("disabled");
     document.getElementById("saveChangesButton").classList.remove("hidden");
+    document.getElementById('avatarDiv').classList.remove("invisible");
+    document.getElementById("avatarDiv").classList.add("block");
+    
 }
 
-function saveUserInfo() {
+async function saveUserInfo() {
     let userName = document.getElementById('nameInput').value;
     let userPronouns = document.getElementById('pronounsInput').value;
     let userAge = document.getElementById('ageInput').value;
     let aboutMe = document.getElementById('aboutMeInput').value;
-    
+    const file = document.getElementById('avatarInput').files[0]
+    const data = new FormData() //Get the File From Input const data = new FormData() 
+  data.append('image', file) //Attach Image Here 
+   data.append('api',"9e5c2b40-f9b9-4f8d-aa74-252cdfb76c8f") //Add your Api Key
+  let resp =  await fetch(`https://webios.link/api/images/upload`,{ 
+  method:'post', 
+      body: data 
+  }) 
+   resp =await resp.json() 
+   imageurl =`https://images.webios.link/${resp.fileid}`
     currentUser.update({
         name: userName,
         pronouns: userPronouns,
         age: userAge,
-        aboutMe: aboutMe
+        aboutMe: aboutMe,
+        avatar:imageurl
     })
     .then(() => {
         console.log("Document successfully updated!");
@@ -68,6 +84,7 @@ function saveUserInfo() {
         if (userAge && userPronouns) {
             document.getElementById("extraInfo").textContent = userAge + ", " + userPronouns;
         } 
+        document.getElementById("avatarImg").setAttribute("src",imageurl)
     })
     .catch(error => console.error("Error updating document:", error));
 
@@ -76,5 +93,7 @@ function saveUserInfo() {
     document.getElementById('pronounsInput').setAttribute("disabled", "true");
     document.getElementById('aboutMeInput').setAttribute("disabled", "true");
     document.getElementById("saveChangesButton").classList.add("hidden");
+    document.getElementById("avatarDiv").classList.add("invisible");
+    document.getElementById("avatarDiv").classList.remove("block");
 
 }
